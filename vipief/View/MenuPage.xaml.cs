b3ae5b61784df33e0415f493cbe17c4bc6bda747
zavashas <144954320+zavashas@users.noreply.github.com>
2024-05-03 14:52:10 +0300

    a

--- vipief/View/MenuPage.xaml.cs
+++ vipief/View/MenuPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,6 +34,11 @@ namespace vipief.View
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new CalendarPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new CalendarPage());
         }
